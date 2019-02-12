6a89f60ecb0f4cb4a53965d0378faaf04aa7f2ad
Freddy Juhel <domaine75@gmail.com>
2019-02-12 18:32:08 +0100

    Initial commit

---
+++ CleanUpAtStartup/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CleanUpAtStartup
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      string temporaryDirectory = "c:\\temp";
+      
+      // for each sub-directories, try to delete every files
+      try
+      {
+
+      }
+      catch (Exception)
+      {
+        // skip file and try next one
+      }
+    }
+  }
+}
